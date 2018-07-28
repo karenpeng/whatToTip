b5e75fe6e8becaec3092951c5ab9ae130dbe02f4
karen punk <karenpenglabs@gmail.com>
2018-07-28 11:32:01 -0700

    add async storage

--- js/components/payment/payment.js
+++ js/components/payment/payment.js
@@ -1,22 +1,39 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, AsyncStorage } from 'react-native';
 import idx from 'idx';
 
 import {calculateTips, getDollarSign} from '../../utils/money-calculator';
 import PaymentControlPanel from './payment-control-panel';
 import PaymentDisplay from './payment-display';
 
+const TIPS_OPTION_KEY = '@MyTipOption';
+
 export default class Payment extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      // TODO: read from local storage
       tipOption: 0.15,
     };
   }
 
-  onTipOptionSelect = tipOption => () => {
+  componentDidMount() {
+    try {
+      const tipOption = await AsyncStorage.getItem(TIPS_OPTION_KEY);
+      if (tipOption !== null) {
+        this.setState({ tipOption });
+      }
+    } catch (error) {
+     console.log(error);
+    }
+  };
+
+  onTipOptionSelect = tipOption => async() => {
     this.setState({ tipOption });
+    try {
+      await AsyncStorage.setItem(TIPS_OPTION_KEY, tipOption);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   render() {
